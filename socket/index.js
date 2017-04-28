25c5551814e10497b260af7b8b96d7e12f0a895e
rkhok55555 <sephirothangel_0210@yahoo.com>
2017-04-27 18:12:07 -0700

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -1,23 +1,21 @@
 const socketIo = require( 'socket.io' )
-const {USER_JOINED, MESSAGE_SEND } = require( '../chat/constants_chat' )
+const { USER_JOINED, MESSAGE_SEND } = require( '../chat/constants_chat' )
 
-const init = (app, server) => {
-     const io = socketIo(server)
+const init = ( app, server ) => {
+  const io = socketIo( server )
 
-     app.set('io', io)
+  app.set( 'io', io )
 
-     io.on('connection', socket => {
-       console.log('client connected')
+  io.on( 'connection', socket => {
+    console.log( 'client connected' )
 
-       socket.on('disconnect', data => {
-         console.log('client disconnected')
-       })
+    socket.on( 'disconnect', data => {
+      console.log( 'client disconnected' )
+    })
 
-       socket.on(USER_JOINED, data => io.emit(USER_JOINED, data))
-       socket.on(MESSAGE_SEND, data => io.emit( MESSAGE_SEND, data))
+    socket.on( USER_JOINED, data => io.emit( USER_JOINED, data ))
+    socket.on( MESSAGE_SEND, data => io.emit( MESSAGE_SEND, data ))
+  })
+}
 
-     }) //end io.on
-}//end init
-
-module.exports = {init}
-
+module.exports = { init }
